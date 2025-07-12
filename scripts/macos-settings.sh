f8b9f93ba7c7b2896f9666e5ff6cb42129ecd22b
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-07-12 22:24:04 +0900

    chore(): defaults

--- scripts/macos-settings.sh
+++ scripts/macos-settings.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -euo pipefail
 
-echo "--- Configuring macOS Dock settings ---"
+echo "--- Configuring macOS settings ---"
 
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock magnification -bool false
@@ -9,6 +9,10 @@ defaults write com.apple.dock orientation -string "left"
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock tilesize -int 60
 
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write com.apple.HIToolbox AppleFnUsageType -int 0
+
 killall Dock
 
-echo "--- Dock configuration completed successfully ---"
+echo "--- Configuration completed successfully ---"
