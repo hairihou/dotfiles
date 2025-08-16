e76c57964512efde5822ecb9c14cc5f92909e90b
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-08-16 15:32:39 +0900

    chore(): docs

--- scripts/macos-settings.sh
+++ scripts/macos-settings.sh
@@ -7,7 +7,7 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock magnification -bool false
 defaults write com.apple.dock orientation -string "left"
 defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock tilesize -int 60
+defaults write com.apple.dock tilesize -int 48
 
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
