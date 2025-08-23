da906dbb40b072ce11ebda4d18cffe2104e2ae5c
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-08-23 20:52:30 +0900

    fix(): defaults

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -13,6 +13,9 @@ defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write com.apple.HIToolbox AppleFnUsageType -int 0
 
+defaults write NSGlobalDomain com.apple.trackpad.scaling -float 3
+defaults write com.apple.AppleMultitouchTrackpad ActuationStrength -int 0
+
 killall Dock
 
 echo "--- Configuration completed successfully ---"
