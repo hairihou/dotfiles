819838014ed8eaa5458591f8808baaab3f0cbb98
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-08-26 00:11:03 +0900

    fix(): cdefaults

--- scripts/cdefaults.sh
+++ scripts/cdefaults.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "--- Configuring defaults ---"
+cdefaults() {
+  defaults write com.apple.dock autohide -bool true
+  defaults write com.apple.dock magnification -bool false
+  defaults write com.apple.dock orientation -string "left"
+  defaults write com.apple.dock show-recents -bool false
+  defaults write com.apple.dock tilesize -int 48
 
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock magnification -bool false
-defaults write com.apple.dock orientation -string "left"
-defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock tilesize -int 48
+  defaults write NSGlobalDomain KeyRepeat -int 2
+  defaults write NSGlobalDomain InitialKeyRepeat -int 15
+  defaults write com.apple.HIToolbox AppleFnUsageType -int 0
 
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-defaults write com.apple.HIToolbox AppleFnUsageType -int 0
+  defaults write NSGlobalDomain com.apple.trackpad.scaling -float 3
+  defaults write com.apple.AppleMultitouchTrackpad ActuationStrength -int 0
 
-defaults write NSGlobalDomain com.apple.trackpad.scaling -float 3
-defaults write com.apple.AppleMultitouchTrackpad ActuationStrength -int 0
+  killall Dock
+}
 
-killall Dock
-
-echo "--- Configuration completed successfully ---"
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && cdefaults "$@"
