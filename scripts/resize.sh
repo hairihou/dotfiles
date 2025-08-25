c6dd2fc347346f02d440df07e8cd39dd92ad0fe3
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2025-08-26 00:33:23 +0900

    chore(): resize

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-resize() {
-  local scale=${1:-1}
-  if ! [[ "$scale" =~ ^[0-9]+(\.[0-9]*)?$ ]]; then
-    echo "Error: Scale must be a number." >&2
-    return 1
-  fi
-  local int_scale=${scale%.*}
-  [[ "$scale" == "$int_scale" ]] && int_scale=$scale
-  ((int_scale < 1)) && int_scale=1
-  ((int_scale > 3)) && int_scale=3
-  printf '\e[8;%d;%dt' $((24 * int_scale)) $((80 * int_scale))
-}
+scale=${1:-1}
 
-[[ "${BASH_SOURCE[0]}" == "${0}" ]] && resize "$@"
+if ! [[ "$scale" =~ ^[0-9]+(\.[0-9]*)?$ ]]; then
+  echo "Error: Scale must be a number." >&2
+  exit 1
+fi
+
+int_scale=${scale%.*}
+[[ "$scale" == "$int_scale" ]] && int_scale=$scale
+
+((int_scale < 1)) && int_scale=1
+((int_scale > 3)) && int_scale=3
+
+printf '\e[8;%d;%dt' $((24 * int_scale)) $((80 * int_scale))
