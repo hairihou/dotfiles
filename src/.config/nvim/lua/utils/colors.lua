681c15cd676bec1b6bb9345e3677446bb3054afd
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-01-30 10:51:36 +0900

    chore(): nvim

---
+++ src/.config/nvim/lua/utils/colors.lua
@@ -0,0 +1,20 @@
+-- https://tailwindcss.com/docs/colors
+local M = {}
+M.slate = {
+  [50] = "#f8fafc",
+  [100] = "#f1f5f9",
+  [200] = "#e2e8f0",
+  [300] = "#cbd5e1",
+  [500] = "#64748b",
+  [700] = "#334155",
+  [800] = "#1e293b",
+  [900] = "#0f172a",
+}
+M.amber = { [300] = "#fcd34d", [400] = "#fbbf24" }
+M.blue = { [400] = "#60a5fa" }
+M.cyan = { [400] = "#22d3ee" }
+M.green = { [400] = "#4ade80" }
+M.pink = { [400] = "#f472b6" }
+M.red = { [400] = "#f87171" }
+M.violet = { [400] = "#a78bfa" }
+return M
