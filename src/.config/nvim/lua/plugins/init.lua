323e84474e9fde859cfd9f1dfc92630f0b0878e2
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-01-30 12:26:32 +0900

    feat(nvim): plugins

---
+++ src/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    opts = {
+      keymap = { preset = "default" },
+      sources = { default = { "lsp", "path", "buffer" } },
+    },
+  },
+})
