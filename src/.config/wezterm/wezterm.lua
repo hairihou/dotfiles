fc308461dcaa0bf6c6b6e6ff81256456d23570b2
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-02-01 00:25:53 +0900

    fix(): wezterm

--- src/.config/wezterm/wezterm.lua
+++ src/.config/wezterm/wezterm.lua
@@ -9,9 +9,9 @@ local theme = {
   pink = { [300] = "#f9a8d4", [400] = "#f472b6" },
   red = { [300] = "#fca5a5", [400] = "#f87171" },
   slate = {
-    [50] = "#f8fafc", [100] = "#f1f5f9", [200] = "#e2e8f0", [300] = "#cbd5e1",
-    [400] = "#94a3b8", [500] = "#64748b", [600] = "#475569", [700] = "#334155",
-    [800] = "#1e293b", [900] = "#0f172a",
+    [100] = "#f1f5f9", [200] = "#e2e8f0", [300] = "#cbd5e1", [400] = "#94a3b8",
+    [500] = "#64748b", [600] = "#475569", [700] = "#334155", [800] = "#1e293b",
+    [900] = "#0f172a",
   },
 }
 config.colors = {
@@ -22,20 +22,20 @@ config.colors = {
   background = theme.slate[900],
   brights = {
     theme.slate[500], theme.red[300], theme.green[300], theme.amber[300],
-    theme.blue[300], theme.pink[300], theme.cyan[300], theme.slate[100],
+    theme.blue[300], theme.pink[300], theme.cyan[300], theme.slate[200],
   },
-  cursor_bg = theme.slate[50],
+  cursor_bg = theme.slate[100],
   cursor_fg = theme.slate[900],
-  foreground = theme.slate[200],
+  foreground = theme.slate[300],
   selection_bg = theme.slate[700],
   selection_fg = theme.slate[100],
   tab_bar = {
     active_tab = { bg_color = theme.slate[700], fg_color = theme.slate[100] },
     background = theme.slate[900],
     inactive_tab = { bg_color = theme.slate[800], fg_color = theme.slate[400] },
-    inactive_tab_hover = { bg_color = theme.slate[700], fg_color = theme.slate[200] },
+    inactive_tab_hover = { bg_color = theme.slate[700], fg_color = theme.slate[300] },
     new_tab = { bg_color = theme.slate[800], fg_color = theme.slate[400] },
-    new_tab_hover = { bg_color = theme.slate[700], fg_color = theme.slate[200] },
+    new_tab_hover = { bg_color = theme.slate[700], fg_color = theme.slate[300] },
   },
 }
 config.default_cursor_style = "SteadyBlock"
@@ -52,7 +52,6 @@ config.native_macos_fullscreen_mode = false
 config.scrollback_lines = 10000
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
-config.use_fancy_tab_bar = false
 config.use_ime = true
 config.warn_about_missing_glyphs = false
 config.window_close_confirmation = "NeverPrompt"
