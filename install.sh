23bf999cc39e74f1dd22aa126052073a62ab23f9
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-03-18 09:31:22 +0900

    refactor(install): flatten install.sh by removing unnecessary functions

--- install.sh
+++ install.sh
@@ -4,23 +4,16 @@ set -euo pipefail
 readonly dst="$HOME/dotfiles"
 readonly src='https://github.com/hairihou/dotfiles.git'
 
-sync_repo() {
-  if [ ! -e "$dst/.git" ]; then
-    git clone "$src" "$dst"
-  elif [ "$(git -C "$dst" config --get remote.origin.url)" != "$src" ]; then
-    echo 'Error: Remote origin URL does not match expected URL'
-    exit 1
-  else
-    echo 'Updating existing repository...'
-    git -C "$dst" fetch --prune
-    git -C "$dst" switch main
-    git -C "$dst" pull origin main
-  fi
-}
+if [ ! -e "$dst/.git" ]; then
+  git clone "$src" "$dst"
+elif [ "$(git -C "$dst" config --get remote.origin.url)" != "$src" ]; then
+  echo 'Error: Remote origin URL does not match expected URL'
+  exit 1
+else
+  echo 'Updating existing repository...'
+  git -C "$dst" fetch --prune
+  git -C "$dst" switch main
+  git -C "$dst" pull origin main
+fi
 
-main() {
-  sync_repo
-  "$dst/bin/linkup"
-}
-
-main
+"$dst/bin/linkup"
