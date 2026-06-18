4ad4ca5cccf779ec789932f2ff9b5d4e35eb18f1
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-06-19 07:45:58 +0900

    fix(nvim): render-markdown keymap

--- src/.config/nvim/lua/plugins/render-markdown.lua
+++ src/.config/nvim/lua/plugins/render-markdown.lua
@@ -3,7 +3,7 @@ return {
   ft = "markdown",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   keys = {
-    { "<leader>md", "<Cmd>RenderMarkdown toggle<CR>", desc = "Markdown Preview" },
+    { "<leader>mp", "<Cmd>RenderMarkdown toggle<CR>", desc = "Markdown Preview" },
   },
   opts = {
     heading = {
