376a2f5ce83615a8e888376468d2d052c70de4de
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-06-25 08:25:27 +0900

    chore()

--- src/.config/nvim/lua/plugins/oil.lua
+++ src/.config/nvim/lua/plugins/oil.lua
@@ -1,22 +1,3 @@
-local git_ignored = setmetatable({}, {
-  __index = function(self, dir)
-    local result = vim
-      .system(
-        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
-        { cwd = dir, text = true }
-      )
-      :wait()
-    local ret = {}
-    if result.code == 0 then
-      for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
-        ret[line:gsub("/$", "")] = true
-      end
-    end
-    rawset(self, dir, ret)
-    return ret
-  end,
-})
-
 return {
   "stevearc/oil.nvim",
   keys = {
@@ -28,20 +9,11 @@ return {
       ["-"] = "actions.parent",
       ["<CR>"] = "actions.select",
       ["<Esc>"] = "actions.close",
-      ["g."] = "actions.toggle_hidden",
     },
     use_default_keymaps = false,
     view_options = {
-      show_hidden = true,
-      is_hidden_file = function(name, bufnr)
-        if name == ".git" then
-          return true
-        end
-        local dir = require("oil").get_current_dir(bufnr)
-        if not dir then
-          return vim.startswith(name, ".") and name ~= ".."
-        end
-        return git_ignored[dir][name] or false
+      is_hidden_file = function(name, _)
+        return name == ".git" or name == ".DS_Store"
       end,
     },
   },
