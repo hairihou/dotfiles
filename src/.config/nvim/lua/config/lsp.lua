1869d5d83dbe381be5a3b8246cf5e51de93b4be5
Yusuke Mizuno <38448411+hairihou@users.noreply.github.com>
2026-07-06 13:53:33 +0900

    fix(): nvim

--- src/.config/nvim/lua/config/lsp.lua
+++ src/.config/nvim/lua/config/lsp.lua
@@ -12,5 +12,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("my.lsp", {}),
   callback = function(ev)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Go to Definition" })
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method("textDocument/documentColor") then
+      vim.lsp.document_color.enable(true, { bufnr = ev.buf })
+    end
   end,
 })
